9d64f99e48b541a5ebda8e8c6b814fb13f3762b9
andr <andreahm@gmx.com>
2022-02-24 10:17:53 -0500

    adding substance

---
+++ port-scanner.py
@@ -0,0 +1,44 @@
+import pyfiglet
+import sys
+import socket
+from datetime import datetime
+from time import sleep
+
+welcome_banner=pyfiglet.figlet_format("Port Scanner")
+print(welcome_banner)
+
+if len(sys.argv) == 2:
+    target=socket.gethostbyname(sys.argv[1])
+else:
+    print("Missing arguments! Did you forget the IP address?")
+
+print("*" * 50)
+print("Scan started...")
+print("Start time: " + str(datetime.now()))
+print("Scanning host: " +target)
+print("*" * 50)
+
+try:
+    for port in range(1,1000):
+        s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(10)
+
+        result=s.connect_ex((target, port))
+        if result == 0:
+            print("Port {} open".format(port))
+        s.close()
+
+except KeyboardInterrupt:
+    print("\nStopping scan...")
+    sleep(5)
+    print("\nScan terminated")
+    sys.exit()
+
+except socket.gaierror:
+    print("\nUnresolved hostname...")
+    sys.exit()
+
+except socket.error:
+    print("\nSpecified host not responding...")
+    sys.exit()
+
